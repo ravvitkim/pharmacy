8278d521d11521d0b7ec42223052ece11fa3ffad
ravvitkim <mirkim6086@naver.com>
2025-09-02 15:57:23 +0900

    KAKAO_API 주소검색 처리

--- src/test/java/com/my/pharmacy/service/KakaoAddressSearchServiceTest.java
+++ src/test/java/com/my/pharmacy/service/KakaoAddressSearchServiceTest.java
@@ -13,6 +13,9 @@ class KakaoAddressSearchServiceTest {
     @Autowired
     KakaoAddressSearchService kakaoAddressSearchService;
 
+    @Autowired
+    KakaoCategorySearchService kakaoCategorySearchService;
+
     @Test
     @DisplayName("API KEY Test")
     void test() {
@@ -27,4 +30,15 @@ class KakaoAddressSearchServiceTest {
                 .requestAddressSearch("강남대로 405");
         System.out.println(dto);
     }
+    @Test
+    @DisplayName("Search Category Test")
+    void categoryTest() {
+        KakaoApiResponseDto dto = new KakaoApiResponseDto();
+        double x = 127.026692446306;
+        double y = 37.4987750083767;
+        double radius = 1000;
+        dto = kakaoCategorySearchService.resultCategorySearch(y, x, radius);
+        System.out.println(dto);
+    }
+
 }
